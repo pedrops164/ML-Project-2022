7680ecacf8fdb469f6db2cc9dc5dcaee606659c3
pedrops164 <A95826@alunos.uminho.pt>
2023-02-16 18:32:16 +0100

    Added MaxPooling functionality. Seems to do worse when a max pooling layer is added

--- PythonNN/CNN.py
+++ PythonNN/CNN.py
@@ -1,4 +1,4 @@
-from Layer import ConvolutionalLayer, ReshapeLayer, Dense
+from Layer import ConvolutionalLayer, ReshapeLayer, Dense, MaxPooling
 from ActivationFunctions import Sigmoid
 from LossFunctions import BCE
 from Adjuster import CNN_Optimizer
@@ -11,10 +11,22 @@ class CNN:
         self.layers = [
             ConvolutionalLayer((1,28,28), 3, 5),
             Sigmoid(),
-            ReshapeLayer((5,26,26), (1, 5*26*26)),
-            Dense(5*26*26, 100, Sigmoid()),
+            ConvolutionalLayer((5,26,26), 3, 5),
+            Sigmoid(),
+            ReshapeLayer((5,24,24), (1, 5*24*24)),
+            Dense(5*24*24, 100, Sigmoid()),
             Dense(100,10, Sigmoid())            
         ]
+        # self.layers = [
+        #     ConvolutionalLayer((1,28,28), 3, 5),
+        #     Sigmoid(),
+        #     MaxPooling((5,26,26), ksize=(2,2)),
+        #     ConvolutionalLayer((5,13,13), 3, 5),
+        #     Sigmoid(),
+        #     ReshapeLayer((5,11,11), (1, 5*11*11)),
+        #     Dense(5*11*11, 100, Sigmoid()),
+        #     Dense(100,10, Sigmoid())            
+        # ]
 
         self.lr = 0.1
 
@@ -49,7 +61,7 @@ class CNN:
             print("error", error / len(Y_pred))
 
 
-def preprocess_data(x, y, limit):
+def preprocess_mnist(x, y, limit):
     x = x.reshape(len(x), 1, 28, 28)
     x = x.astype("float32") / 255
     y = np_utils.to_categorical(y)
@@ -58,14 +70,20 @@ def preprocess_data(x, y, limit):
 
 def cnn_mnist():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train, y_train = preprocess_data(x_train, y_train, 1000)
-    x_test, y_test = preprocess_data(x_test, y_test, 1000)
+    x_train, y_train = preprocess_mnist(x_train, y_train, 1000)
+    x_test, y_test = preprocess_mnist(x_test, y_test, 1000)
 
     cnn = CNN()
     cnn.train(x_train, y_train, 20)
 
     test_error = 0
+    accuracy = 0
     for x, y in zip(x_test, y_test):
         y_pred = cnn.forward(x)
+        prediction = np.argmax(y_pred)
+        true_prediction = np.argmax(y)
+        if prediction == true_prediction:
+            accuracy += 1
         test_error += cnn.loss.calculate(y_pred, y)
     print(test_error / len(y_pred))
+    print("accuracy: ", accuracy / (len(x_test)))
--- PythonNN/Layer.py
+++ PythonNN/Layer.py
@@ -3,67 +3,67 @@ from scipy import signal
 
 
 class Layer:
-	#Initializing weights and biases
-	def __init__(self, n_inputs, n_neurons, activ_function=None):
-		self.n_inputs = n_inputs
-		self.n_neurons = n_neurons
+    #Initializing weights and biases
+    def __init__(self, n_inputs, n_neurons, activ_function=None):
+        self.n_inputs = n_inputs
+        self.n_neurons = n_neurons
 
-	def forward(self, inputs):
-		pass
-		
+    def forward(self, inputs):
+        pass
+        
 
-	def backprop(self, gradient):
-		pass
+    def backprop(self, gradient):
+        pass
 
-	def update(self, optimizer):
-		pass
+    def update(self, optimizer):
+        pass
 
 
 
 class Dense(Layer):
-	#Initializing weights and biases
-	def __init__(self, n_inputs, n_neurons, activ_function=None):
-		limit = 0.05
-		self.n_inputs = n_inputs
-		self.n_neurons = n_neurons
-		self.weights = np.random.uniform(-limit, limit, size=(n_inputs,n_neurons))
-		self.biases = np.zeros((1, n_neurons))
-		self.activ = activ_function
-		self.momentums_weight = np.zeros_like(self.weights)
-		self.momentums_bias = np.zeros_like(self.biases)
+    #Initializing weights and biases
+    def __init__(self, n_inputs, n_neurons, activ_function=None):
+        limit = 0.05
+        self.n_inputs = n_inputs
+        self.n_neurons = n_neurons
+        self.weights = np.random.uniform(-limit, limit, size=(n_inputs,n_neurons))
+        self.biases = np.zeros((1, n_neurons))
+        self.activ = activ_function
+        self.momentums_weight = np.zeros_like(self.weights)
+        self.momentums_bias = np.zeros_like(self.biases)
 
-	def forward(self, inputs):
-		self.input = inputs
-		Z1 = np.dot(inputs, self.weights) + self.biases
-		if self.activ == None:
-			self.output = Z1
-		else:
-			self.activ.forward(Z1)
-			A1 = self.activ.output
-			self.output = A1
-		
+    def forward(self, inputs):
+        self.input = inputs
+        Z1 = np.dot(inputs, self.weights) + self.biases
+        if self.activ == None:
+            self.output = Z1
+        else:
+            self.activ.forward(Z1)
+            A1 = self.activ.output
+            self.output = A1
+        
 
-	def backprop(self, gradient):
-		if self.activ != None:
-			self.activ.backprop(gradient)
-			gradient = self.activ.inputs_deriv
+    def backprop(self, gradient):
+        if self.activ != None:
+            self.activ.backprop(gradient)
+            gradient = self.activ.inputs_deriv
 
-		self.weights_deriv = np.dot(self.input.T, gradient)
-		self.biases_deriv = np.sum(gradient, axis=0, keepdims=True)
+        self.weights_deriv = np.dot(self.input.T, gradient)
+        self.biases_deriv = np.sum(gradient, axis=0, keepdims=True)
 
-		# we calculate derivative of inputs to pass to the previous layer while back propagating
-		self.inputs_deriv = np.dot(gradient, self.weights.T)
+        # we calculate derivative of inputs to pass to the previous layer while back propagating
+        self.inputs_deriv = np.dot(gradient, self.weights.T)
 
-	def reset_params(self):
-		limit = 0.05
-		self.weights = np.random.uniform(-limit, limit, size=(self.n_inputs,self.n_neurons))
-		self.biases = np.zeros((1, self.n_neurons))
-		self.momentums_weight = np.zeros_like(self.weights)
-		self.momentums_bias = np.zeros_like(self.biases)
+    def reset_params(self):
+        limit = 0.05
+        self.weights = np.random.uniform(-limit, limit, size=(self.n_inputs,self.n_neurons))
+        self.biases = np.zeros((1, self.n_neurons))
+        self.momentums_weight = np.zeros_like(self.weights)
+        self.momentums_bias = np.zeros_like(self.biases)
 
-	def update(self, optimizer):
-		self.weights -= optimizer.lr * self.weights_deriv
-		self.biases -= optimizer.lr * self.biases_deriv
+    def update(self, optimizer):
+        self.weights -= optimizer.lr * self.weights_deriv
+        self.biases -= optimizer.lr * self.biases_deriv
 
 # Convolutional Layer
 class ConvolutionalLayer(Layer):
@@ -116,3 +116,73 @@ class ReshapeLayer(Layer):
 
     def backprop(self, gradient):
         self.inputs_deriv = np.reshape(gradient, self.input_shape)
+
+
+class MaxPooling(Layer):
+    def __init__(self, input_shape, ksize=(2,2)):
+        self.ksize = ksize
+        self.input_shape = input_shape
+
+    def forward(self, input):
+        self.input = input
+        self.output = pooling(input, self.ksize, pad=True)
+
+    def backprop(self, gradient):
+        # inputs deriv has the shape of the inputs.
+        # for each region, the derivative for the maximum element is 1, and the rest is 0.
+        # we have to multiply each value by the gradient, therefore the maximum element will
+        # have the value of the gradient in that spot.
+        self.inputs_deriv = np.zeros(self.input_shape)
+        depth = self.input_shape[0] # input depth
+        height_out = self.output.shape[0]
+        width_out = self.output.shape[1]
+        pool_height = self.ksize[0]
+        pool_width = self.ksize[1]
+        for d in range(depth):
+            for h in range(height_out):
+                for w in range(width_out):
+                    h_max, w_max = np.where(np.max(self.input[d, h*pool_height : (h+1)*pool_height, w*pool_width : (w+1)*pool_width]) \
+                       == self.input[d, h*pool_height : (h+1)*pool_height, w*pool_width : (w+1)*pool_width])
+                    h_max = h_max[0]
+                    w_max = w_max[0]
+                    self.inputs_deriv[d, h*pool_height : (h+1)*pool_height, w*pool_width : (w+1)*pool_width][h_max][w_max] \
+                        = gradient[d, h, w]
+
+def pooling(mat,ksize,method='max',pad=False):
+    """
+    Non-overlapping pooling on 2D or 3D data
+    <mat>: ndarray, input array to pool.
+    <ksize>: tuple of 2, kernel size in (ky, kx).
+    <method>: str, 'max for max-pooling, 
+                   'mean' for mean-pooling.
+    <pad>: bool, pad <mat> or not. If no pad, output has size
+           n//f, n being <mat> size, f being kernel size.
+           if pad, output has size ceil(n/f).
+
+    Return <result>: pooled matrix.
+    """
+
+    m, n = mat.shape[1:] # ignore the number of inputs
+    ky,kx=ksize
+
+    _ceil=lambda x,y: int(np.ceil(x/float(y)))
+
+    if pad:
+        ny=_ceil(m,ky)
+        nx=_ceil(n,kx)
+        size=mat.shape[:1]+(ny*ky, nx*kx)
+        mat_pad=np.full(size,np.nan)
+        mat_pad[:m,:n,...]=mat
+    else:
+        ny=m//ky
+        nx=n//kx
+        mat_pad=mat[:ny*ky, :nx*kx, ...]
+
+    new_shape=mat.shape[:1]+(ny,ky,nx,kx)
+
+    if method=='max':
+        result=np.nanmax(mat_pad.reshape(new_shape),axis=(2,4))
+    else:
+        result=np.nanmean(mat_pad.reshape(new_shape),axis=(2,4))
+
+    return result
